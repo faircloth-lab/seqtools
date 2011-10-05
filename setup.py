aa157df6a20dfdc43f819817abe65ac57b43b0f2
Brant Faircloth <brant.faircloth@gmail.com>
2011-10-05 14:02:46 -0700

    update requires= and license=

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         author="Brant Faircloth",
         author_email="brant.faircloth+seqtools@gmail.com ",
         url="http://github.com/faircloth-lab/seqtools/",
-        license="http://www.opensource.org/licenses/BSD-3-Clause",
+        license="",
         classifiers=[
             'Development Status :: 4 - Beta',
             'Environment :: Console',
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             'Programming Language :: Python',
             'Topic :: Scientific/Engineering :: Bio-Informatics',
              ],
-        requires=['NumPy (>=1.3)',],
+        requires=['numpy>=1.3',],
         long_description=open('README.rst').read(),
         packages = find_packages('seqtools'),
         package_dir = {'':'seqtools'},
