3e3641f76139de7eb84e1c738e0b308c050cfe4b
Brant Faircloth <brant@faircloth-lab.org>
2013-04-20 19:56:23 -0700

    use BufferedReader to speed up gzip parsing (substantially)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 if __name__ == '__main__':
     setup(
         name='seqtools',
-        version="0.7",
+        version="0.8",
         description="Tools for sequence and alignment manipulation",
         author="Brant Faircloth",
         author_email="brant.faircloth+seqtools@gmail.com ",
